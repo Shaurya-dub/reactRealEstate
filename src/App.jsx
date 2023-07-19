09d329d59aa0e8543aeeaf724cb411dcbd3b9252
Shaurya <shauryadubey@hotmail.ca>
2023-07-19 12:52:31 -0600

    Banner component basics done

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,51 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Link from 'next/link'
+import Image from 'next/image'
+import {Flex,Bow, Text, Button, Box} from '@chakra-ui/react'
 import './App.css'
+
+const Banner = ({purpose,imageUrl,title1,title2,desc1,desc2,linkName,buttonText}) => (
+  <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
+<Image src={imageUrl} width={500} height={300} alt='banner' />
+<Box p="5">
+  <Text color="gray.500" fontSize="sm" fontWeight="medium">{purpose}</Text>
+  <Text fontSize="3xl" fontWeight="bold">{title1}<br/>{title2}</Text>
+  <Text fontSize="lg" paddingTop='3' paddingBottom='3'>{desc1} <br/>{desc2}</Text>
+  <Button fontSize="xl" bg="blue.300" color="white">
+    <Link href={linkName}>{buttonText}</Link>
+  </Button>
+</Box>
+  </Flex>
+)
+
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+  <Banner
+  purpose="Rent a Home"
+  title1="Rental Homes For"
+  title2="Everyone"
+  desc1="Explore Apartments, Villas, Homes"
+  desc2="And More"
+  buttonText="Explore Renting"
+  linkName="/search?purpose=for-rent"
+  imageUrl="bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
+  />
+  <Banner
+  purpose="Buy a Home"
+  title1="Find, Buy, and Own Your"
+  title2="Dream Home"
+  desc1="Explore Apartments, Villas, Homes"
+  desc2="And More"
+  buttonText="Explore Buying"
+  linkName="/search?purpose=for-sale"
+  imageUrl="bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
+  />
     </>
   )
 }
